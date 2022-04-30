415ed6c8594bdb6d27fbf68ab5f4f0998ba90581
luciiii6 <73104024+luciiii6@users.noreply.github.com>
2022-04-30 13:37:12 +0300

    GET /captions responds with static body

--- app/controllers/captions_controller.rb
+++ app/controllers/captions_controller.rb
@@ -2,6 +2,15 @@
 
 class CaptionsController < ApplicationController
   def index
-    head :ok
+    captions = [
+      {
+        id: 123,
+        url: "http://image.url/meme.jpg",
+        text: "caption text",
+        caption_url: ""
+      }
+    ]
+    render json: { captions: }
+    # head :ok
   end
 end
--- spec/requests/captions_spec.rb
+++ spec/requests/captions_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe "Captions", type: :request do
       get "/captions"
       expect(response).to have_http_status(:ok)
     end
+
+    it 'responds with correct body' do
+      get '/captions'
+      json_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json_response).to eq({ captions: [
+                                    {
+                                      id: 123,
+                                      url: "http://image.url/meme.jpg",
+                                      text: "caption text",
+                                      caption_url: ""
+                                    }
+                                  ] })
+    end
   end
 end
