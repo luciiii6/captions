c3ed5bc0687bfc235a6a02ceef16d2220e57c30a
luciiii6 <73104024+luciiii6@users.noreply.github.com>
2022-04-30 13:41:44 +0300

    Changed spec to use route helpers

--- spec/requests/captions_spec.rb
+++ spec/requests/captions_spec.rb
@@ -5,12 +5,12 @@ require 'rails_helper'
 RSpec.describe "Captions", type: :request do
   describe "GET /captions" do
     it "responds with 200" do
-      get "/captions"
+      get captions_path
       expect(response).to have_http_status(:ok)
     end
 
     it 'responds with correct body' do
-      get '/captions'
+      get captions_path
       json_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(json_response).to eq({ captions: [
